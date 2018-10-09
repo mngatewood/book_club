ba5c093b6ba959c4d213b237737f7abf1c60023b
Michael Gatewood <michael@mngatewood.com>
2018-10-09 13:58:13 -0600

    Updates schema with book reviews relationship

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181009192357) do
+ActiveRecord::Schema.define(version: 20181009195231) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,8 @@ ActiveRecord::Schema.define(version: 20181009192357) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id"
+    t.bigint "book_id"
+    t.index ["book_id"], name: "index_reviews_on_book_id"
     t.index ["user_id"], name: "index_reviews_on_user_id"
   end
 
@@ -40,5 +42,6 @@ ActiveRecord::Schema.define(version: 20181009192357) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "reviews", "books"
   add_foreign_key "reviews", "users"
 end
