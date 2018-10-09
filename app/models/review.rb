a37321edb6b38bcb80a8722bbc106abdb46f3575
Michael Gatewood <michael@mngatewood.com>
2018-10-09 13:58:30 -0600

    Adds book relationship to review

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,7 +1,8 @@
 class Review < ApplicationRecord
 
-  validates_presence_of :title, :review, :rating, :user_id
+  validates_presence_of :title, :review, :rating, :user_id, :book_id
 
   belongs_to :user
+  belongs_to :book
 
 end
