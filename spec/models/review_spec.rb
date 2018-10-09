9fa1692916ad13966fd766a2ccf374767b497721
Michael Gatewood <michael@mngatewood.com>
2018-10-09 13:57:24 -0600

    Adds validation and relationship tests for review

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -7,10 +7,11 @@ describe Review, type: :model do
     it { should validate_presence_of(:review) }
     it { should validate_presence_of(:rating) }
     it { should validate_presence_of(:user_id) }
-    # it { should validate_presence_of(:book_id) }
+    it { should validate_presence_of(:book_id) }
   end
 
   describe 'Relationships' do
     it { should belong_to(:user) }
+    it { should belong_to(:book) }
   end
 end
