74e5722fa42dd8ed4435605f5cfe7e9d02032a2b
Quinn Krug <quinn.krug@gmail.com>
2018-10-09 19:33:25 -0600

    BookAuthor built and tested

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,5 +3,7 @@ class Book < ApplicationRecord
   validates_presence_of :title, :author, :page_count, :year_published
 
   has_many :reviews
+  has_many :book_authors
+  has_many :authors, through: :book_authors
 
 end
