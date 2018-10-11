eb551caa7b6571b6a425c0aa9671919e044d5192
Michael Gatewood <michael@mngatewood.com>
2018-10-10 21:20:32 -0600

    Replaces all hard coded ids with dynamic references

--- spec/features/user_sees_all_reviews_for_given_user_spec.rb
+++ spec/features/user_sees_all_reviews_for_given_user_spec.rb
@@ -3,44 +3,50 @@ require 'rails_helper'
 describe "As a vistitor" do
 
     before(:each) do
-
       @user_1 = User.create(name: "John")
       @user_2 = User.create(name: "Jane")
       @user_3 = User.create(name: "David")
       @book_1 = Book.create(title: "Black Beauty", page_count: 255, year_published: 1877)
-      @review_1 = @user_1.reviews.create(title: "Flirtation", review: "There are ways.", rating: 5, book_id: 1)
-      @review_2 = @user_1.reviews.create(title: "Your best friend until you find out he’s a drug dealer", review: "The Great Gatsby is your neighbor.", rating: 5, book_id: 1)
-      @review_3 = @user_1.reviews.create(title: "Great story", review: "It is a great story.", rating: 3, book_id: 1)
-      @review_4 = @user_2.reviews.create(title: "Who says psychopaths aren’t romantic?", review: "If I saw you everyday, forever, I would remember this time.", rating: 3, book_id: 1)
-      @review_5 = @user_2.reviews.create(title: "Needs an editor.", review: "David Goodis needs an editor.", rating: 3, book_id: 1)
-      @review_6 = @user_2.reviews.create(title: "A hefty volume", review: "This is a hefty and imposing volume.", rating: 4, book_id: 1)
-      @review_7 = @user_3.reviews.create(title: "Must read", review: "The idea that there exists such thing as a “must read” book.", rating: 5, book_id: 1)
-      @review_8 = @user_3.reviews.create(title: "Expansive", review: "White Teeth is an expansive, detailed, and beautifully written attempt.", rating: 3, book_id: 1)
+      @book_2 = Book.create(title: "Black Friday", page_count: 133, year_published: 1986)
+      @review_1 = @user_1.reviews.create(title: "Flirtation", review: "There are ways.", rating: 5, book_id: @book_1.id)
+      @review_2 = @user_1.reviews.create(title: "Your best friend until you find out he’s a drug dealer", review: "The Great Gatsby is your neighbor.", rating: 5, book_id: @book_1.id)
+      @review_3 = @user_1.reviews.create(title: "Great story", review: "It is a great story.", rating: 3, book_id: @book_1.id)
+      @review_4 = @user_2.reviews.create(title: "Who says psychopaths aren’t romantic?", review: "If I saw you everyday, forever, I would remember this time.", rating: 3, book_id: @book_1.id)
+      @review_5 = @user_2.reviews.create(title: "Needs an editor.", review: "David Goodis needs an editor.", rating: 3, book_id: @book_1.id)
+      @review_6 = @user_2.reviews.create(title: "A hefty volume", review: "This is a hefty and imposing volume.", rating: 4, book_id: @book_1.id)
+      @review_7 = @user_3.reviews.create(title: "Must read", review: "The idea that there exists such thing as a “must read” book.", rating: 2, book_id: @book_2.id)
+      @review_8 = @user_3.reviews.create(title: "Expansive", review: "White Teeth is an expansive, detailed, and beautifully written attempt.", rating: 1, book_id: @book_2.id)
     end
 
   describe "When I visit user show page" do
 
-    xit 'should see all reviews for the given user' do
+    it 'should show all reviews for the given user' do
 
-      visit '/users/1'
+      visit "/users/#{@user_1.id}"
+
+      save_and_open_page
 
       within("#user-heading") do
-        expect(page).to have_content("John")
+        expect(page).to have_content(@user_1.name)
       end
 
       within("#user-reviews-container") do
-            # binding.pry
         expect(page).to have_content("Title: #{@review_1.title}")
         expect(page).to have_content("Title: #{@review_2.title}")
         expect(page).to have_content("Title: #{@review_3.title}")
+        expect(page).to_not have_content("Title: #{@review_7.title}")
+        expect(page).to_not have_content("Title: #{@review_8.title}")
         expect(page).to have_content("Rating: #{@review_1.rating}")
         expect(page).to have_content("Rating: #{@review_2.rating}")
         expect(page).to have_content("Rating: #{@review_3.rating}")
+        expect(page).to_not have_content("Rating: #{@review_7.rating}")
+        expect(page).to_not have_content("Rating: #{@review_8.rating}")
         expect(page).to have_content(@review_1.review)
         expect(page).to have_content(@review_2.review)
         expect(page).to have_content(@review_3.review)
+        expect(page).to_not have_content(@review_7.review)
+        expect(page).to_not have_content(@review_8.review)
       end
-
     end
   end
 end
