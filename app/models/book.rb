d004781ba2144ea34ebb52e13074d57af24868a8
Quinn Krug <quinn.krug@gmail.com>
2018-10-11 11:05:00 -0600

    refactorered tests

--- app/models/book.rb
+++ app/models/book.rb
@@ -7,11 +7,11 @@ class Book < ApplicationRecord
   has_many :authors, through: :book_authors
 
   def average_rating
-    reviews.average(:rating).to_f.round(1)
+    reviews.average(:rating).round(1)
   end
 
   def other_authors(author_id)
-    other_authors = authors.where.not(id: author_id).map(&:name)
+    other_authors = authors.where.not(id: author_id).pluck(:name)
     other_authors.count > 0 ? other_authors : ["None"]
   end
 
--- app/models/review.rb
+++ app/models/review.rb
@@ -5,8 +5,4 @@ class Review < ApplicationRecord
   belongs_to :user
   belongs_to :book
 
-  def user_name
-    User.find(user_id).name
-  end
-
 end
