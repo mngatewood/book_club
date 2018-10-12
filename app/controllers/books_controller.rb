8e8bb952d9cd49f95bd524e40c9a0568e1894b38
Michael Gatewood <michael@mngatewood.com>
2018-10-12 13:05:40 -0600

    Added additional routes for desc sorts

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,14 +1,22 @@
 class BooksController < ApplicationController
   def index
-    if params[:sort] == "pages"
+    if params[:sort] == "pages-asc"
       @books = Book.order(:page_count)
-    elsif params[:sort] == "rating"
+    elsif params[:sort] == "pages-desc"
+      @books = Book.order(page_count: :desc)
+    elsif params[:sort] == "rating-asc"
       @books = Book.sort_by_average_rating
-    elsif params[:sort] == "reviews"
+    elsif params[:sort] == "rating-desc"
+      @books = Book.sort_by_average_rating.reverse
+    elsif params[:sort] == "reviews-asc"
       @books = Book.sort_by_ratings_count
+    elsif params[:sort] == "reviews-desc"
+      @books = Book.sort_by_ratings_count.reverse
     else
       @books = Book.all
   end
+  @top_three_books = Book.top_three_books
+  @bottom_three_books = Book.bottom_three_books
   @top_users = User.most_active_users
   end
 
