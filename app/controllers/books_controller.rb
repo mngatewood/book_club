06901e6b7c2e14326070f55e50526ef9701f743b
Michael Gatewood <michael@mngatewood.com>
2018-10-11 21:02:47 -0600

    Adds params conditionals to index method

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,15 @@
 class BooksController < ApplicationController
   def index
-    @books = Book.all
+    if params[:sort] == "pages"
+      @books = Book.order(:page_count)
+    elsif params[:sort] == "rating"
+      @books = Book.sort_by_average_rating
+    elsif params[:sort] == "reviews"
+      @books = Book.sort_by_ratings_count
+    else
+      @books = Book.all
+  end
+  @top_users = User.most_active_users
   end
 
   def show
