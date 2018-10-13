4b0ee109a81e9f18d8e84411d0ab46fec7110377
Michael Gatewood <michael@mngatewood.com>
2018-10-13 17:55:42 -0600

    Refactors get user id and moves from reviews controller to user model

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -6,24 +6,12 @@ class ReviewsController < ApplicationController
   end
 
   def create
-    user_id = get_user_id(review_params[:username])
+    user_id = User.get_user_id(review_params[:username])
     review = Review.new(review_params.merge(user_id: user_id))
     if review.save
       redirect_to "/books/#{review.book_id}"
     else
       render :new
-    end
-  end
-
-  def get_user_id(name)
-    existing_user_id = User.get_id_from_name(name)
-    # if name exists in users, get the id
-    if existing_user_id
-      return existing_user_id
-    # otherwise, create new user and get the id
-    else
-      new_user = User.create(name: review_params[:username])
-      @user_id = new_user.id
     end
   end
 
