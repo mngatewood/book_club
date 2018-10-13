a6a91e6e694c00ae53c37095285e09e91a9b6dc2
Michael Gatewood <michael@mngatewood.com>
2018-10-13 14:52:36 -0600

    Adds get ids from author names method

--- app/models/author.rb
+++ app/models/author.rb
@@ -4,4 +4,15 @@ class Author < ApplicationRecord
 
   has_many :book_authors
   has_many :books, through: :book_authors
+
+  def self.get_ids_from_names(author_names)
+    author_names.reduce([]) do |id_array, author_name|
+      if Author.exists?(name: author_name.strip)
+        id_array << Author.find_by(name: author_name.strip).id
+      else
+        id_array << Author.create(name: author_name.strip).id
+      end
+    end
+  end
+
 end
