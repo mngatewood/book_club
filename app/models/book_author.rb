7897cc6f3c306b4cd6d0b06784c3002d8fd68f21
Michael Gatewood <michael@mngatewood.com>
2018-10-13 14:51:23 -0600

    Adds add author to book method

--- app/models/book_author.rb
+++ app/models/book_author.rb
@@ -1,4 +1,11 @@
 class BookAuthor < ApplicationRecord
   belongs_to :book
   belongs_to :author
+
+  def self.add_authors_to_book(book_id, author_ids)
+    author_ids.each do |author_id|
+      BookAuthor.create(book_id: book_id, author_id: author_id)
+    end
+  end
+
 end
