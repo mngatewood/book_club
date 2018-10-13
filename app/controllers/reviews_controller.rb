b05a590816765958a8b9fc848c4cdd6178c3379f
Michael Gatewood <michael@mngatewood.com>
2018-10-12 23:23:16 -0600

    Adds get user id method

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -6,8 +6,9 @@ class ReviewsController < ApplicationController
   end
 
   def create
-    # book = Book.find(params[:book])
-    review = Review.new(review_params)
+    user_id = get_user_id(review_params[:username])
+    review = Review.new(review_params.merge(user_id: user_id))
+    # binding.pry
     if review.save
       redirect_to "/books/#{review.book_id}"
     else
@@ -15,10 +16,23 @@ class ReviewsController < ApplicationController
     end
   end
 
+  def get_user_id(name)
+    existing_user_id = User.get_id_from_name(name)
+    # if name exists in users, get the id
+    if existing_user_id
+      return existing_user_id
+    # otherwise, create new user and get the id
+    else
+      new_user = User.create(name: review_params[:username])
+      @user_id = new_user.id
+    end
+
+  end
+
   private
 
   def review_params
-    params.require(:review).permit(:title, :user_id, :rating, :review, :book_id)
+    params.require(:review).permit(:title, :username, :rating, :review, :book_id)
   end
 
 end
