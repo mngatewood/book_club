2762d4493e6f01f875fa296000976bb17ca239c0
Michael Gatewood <michael@mngatewood.com>
2018-10-13 14:52:17 -0600

    Adds tests for get id from author name method

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -5,8 +5,29 @@ describe Author, type: :model do
   describe 'Validations' do
     it { should validate_presence_of(:name)}
   end
+
   describe 'Relationship' do
     it { should have_many(:book_authors) }
     it { should have_many(:books).through(:book_authors)}
+  end
+
+  describe 'Methods' do
+
+    it 'should return an array of author ids given an array of author names' do
+    
+      DatabaseCleaner.strategy = :truncation
+      DatabaseCleaner.clean
+
+      @author_1 = Author.create(name: "John Smith")
+      @author_2 = Author.create(name: "Amy Adams")
+
+      @author_name_1 = "John Smith"
+      @author_name_2 = "James Washington"
+
+      author_ids = Author.get_ids_from_names([@author_name_1, @author_name_2])
+
+      expect(author_ids).to eq([@author_1.id, (@author_2.id + 1)])
+
+    end
   end
 end
