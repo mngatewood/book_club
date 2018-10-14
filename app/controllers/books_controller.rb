65cb26d9bcc585579ccdc7d38404dd21dd92e390
Michael Gatewood <michael@mngatewood.com>
2018-10-13 21:46:30 -0600

    Refactors sort params conditionals

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,16 +1,17 @@
 class BooksController < ApplicationController
   def index
-    if params[:sort] == "pages-asc"
+    @sort = params[:sort]
+    if @sort == "pages-asc"
       @books = Book.order(:page_count)
-    elsif params[:sort] == "pages-desc"
+    elsif @sort == "pages-desc"
       @books = Book.order(page_count: :desc)
-    elsif params[:sort] == "rating-asc"
+    elsif @sort == "rating-asc"
       @books = Book.sort_by_average_rating
-    elsif params[:sort] == "rating-desc"
+    elsif @sort == "rating-desc"
       @books = Book.sort_by_average_rating.reverse
-    elsif params[:sort] == "reviews-asc"
+    elsif @sort == "reviews-asc"
       @books = Book.sort_by_ratings_count
-    elsif params[:sort] == "reviews-desc"
+    elsif @sort == "reviews-desc"
       @books = Book.sort_by_ratings_count.reverse
     else
       @books = Book.all
@@ -18,7 +19,6 @@ class BooksController < ApplicationController
   @top_three_books = Book.top_three_books
   @bottom_three_books = Book.bottom_three_books
   @top_users = User.most_active_users
-  @sort = params[:sort]
   end
 
   def show
