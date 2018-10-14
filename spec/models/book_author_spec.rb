86316e9204a2da5ea066e13b3c8ddba6c34545a1
Michael Gatewood <michael@mngatewood.com>
2018-10-13 19:47:32 -0600

    Adds tests for author model mehtods

--- spec/models/book_author_spec.rb
+++ spec/models/book_author_spec.rb
@@ -9,11 +9,13 @@ describe BookAuthor, type: :model do
 
   describe 'Methods' do
 
-    it 'should create book relationships for each given author' do
-
+    before(:each) do
       @book = Book.create(title: "Black Beauty", page_count: 255, year_published: 1877)
       @author_1 = Author.create(name: "Martin")
       @author_2 = Author.create(name: "Steve")
+    end
+
+    it "should create book relationships for each given author" do
 
       BookAuthor.add_authors_to_book(@book.id, [@author_1.id, @author_2.id])
 
@@ -24,7 +26,46 @@ describe BookAuthor, type: :model do
       expect(BookAuthor.last.author_id).to eq(@author_2.id)
 
     end
+    
+    it "should return true if a book or author exists" do
+
+      BookAuthor.add_authors_to_book(@book.id, [@author_1.id, @author_2.id])
+
+      expect(BookAuthor.book_has_author?(@book.id)).to eq(true)
+      expect(BookAuthor.author_has_book?(@author_1.id)).to eq(true)
+
+    end
+      
+    it "should return false if a book or author does not exist" do
+
+      expect(BookAuthor.book_has_author?(@book.id)).to eq(false)
+      expect(BookAuthor.author_has_book?(@author_1.id)).to eq(false)
+
+    end
+
+    it "should destroy all book author relationships for a given author" do
+
+      BookAuthor.add_authors_to_book(@book.id, [@author_1.id, @author_2.id])
+      BookAuthor.destroy_all_bookauthors_for_author(@author_1)
+
+      expect(BookAuthor.where(:author_id => @author_1.id).count).to eq(0)
+
+    end
+
+    it "should destroy a book if it has no authors" do
+
+      BookAuthor.add_authors_to_book(@book.id, [@author_1.id, @author_2.id])
+      books = BookAuthor.destroy_all_bookauthors_for_author(@author_1)
+      BookAuthor.destroy_all_orphaned_books(books)
+
+      expect(Book.find(@book.id)).to eq(@book)
+
+      orphaned_books = BookAuthor.destroy_all_bookauthors_for_author(@author_2)
+      BookAuthor.destroy_all_orphaned_books(orphaned_books)
+
+      expect(BookAuthor.where(:author_id => @author_2.id).count).to eq(0)
+
+    end
 
   end
-
 end
