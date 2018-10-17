7a701a1c41791171788b492591ad334aa9046d76
Michael Gatewood <michael@mngatewood.com>
2018-10-16 19:35:31 -0600

    Updates controller and view page to sort by rating

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,10 @@ class UsersController < ApplicationController
       @reviews = @user.reviews.order(created_at: :desc)
     elsif @sort == "date-asc"
       @reviews = @user.reviews.order(created_at: :asc)
+    elsif @sort == "rating-desc"
+      @reviews = @user.reviews.order(rating: :desc, created_at: :desc)
+    elsif @sort == "rating-asc"
+      @reviews = @user.reviews.order(rating: :asc, created_at: :asc)
     else
       @reviews = @user.reviews
     end
